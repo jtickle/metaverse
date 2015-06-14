d4e45d3250c444fa4c5871aa87884cb297cf09af
Daniel West <westwolf24@gmail.com>
2015-06-14 19:14:57 -0400

    Transforms
        + Transforms are applied as function of the current time and the
        elapsed time since last frame
        + TODO: already needed an absolute time reference rethink this...
        + TODO: figure out how to actually use js/apply because the nth
        thing is already old

--- src-cljs/metaverse/core.cljs
+++ src-cljs/metaverse/core.cljs
@@ -5,10 +5,19 @@
             [metaverse.gui :as gui])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
-(defn render [ts pts renderer scene camera]
-  (js/requestAnimationFrame #(render % ts renderer scene camera))
-  (gui/set-fps (/ 1000 (- ts pts)))
-  (.render renderer scene camera))
+(defn render [state]
+  (let [{:keys [ts pts renderer scene camera transforms]} state
+        dts (- ts pts)]
+    (doseq [t @transforms]
+      (t ts dts))
+    (js/requestAnimationFrame #(render {:ts %
+                                        :pts ts
+                                        :renderer renderer
+                                        :scene scene
+                                        :camera camera
+                                        :transforms transforms}))
+    (gui/set-fps (/ 1000 dts))
+    (.render renderer scene camera)))
 
 (defn add-single-cube [c]
   (let [geom (js/THREE.BoxGeometry. 1 1 1)
@@ -27,6 +36,40 @@
           {:0x0000 {["rotation" "x"] #(+ % 0.1)
                     ["rotation" "y"] #(+ % 0.1)}}})))
 
+(defn grow-shrink [c]
+  (go
+   (>! c {:transforms
+          {:0x0000 {["scale"]
+                    (fn [prop t dt]
+                      (let [s (+ 0.17 (Math/abs (Math/sin (/ (js/Date.now) 500))))]
+                        (repeat 3 s)))
+                    ["rotation" "x"] 
+                    (fn [prop t dt]
+                      (+ prop 0.1))
+                    ["rotation" "y"]
+                    (fn [prop t dt]
+                      (+  prop 0.1))}}})))
+
+(defn auto-get [mesh path]
+  (if (sequential? path)
+    (apply aget mesh path)
+    (aget mesh path)))
+
+(defn auto-set [mesh path value]
+  (let [prop (auto-get mesh path)]
+    (if (not= (type prop) js/THREE.Vector3)
+      (apply aset
+        (concat [mesh]
+          (if (sequential? path)
+            path
+           [path])
+          [value]))
+      (do 
+        ;;(.log js/console "here" prop (.-set prop))
+        ;; todo(daniel): this is fucked, look at it later
+        ;;(js/Function.prototype.apply prop (.-set prop) (clj->js value))))))
+        (.set prop (nth value 0) (nth value 1) (nth value 2))))))
+
 (defn main []
   (let [render-state (chan)
         scene (js/THREE.Scene.)
@@ -36,7 +79,9 @@
                    (aget js/window "innerHeight"))
                 0.1
                 1000)
-        renderer (js/THREE.WebGLRenderer.)]
+        renderer (js/THREE.WebGLRenderer.)
+        ;; transforms
+        tns      (atom [])]
     
     ;; TODO(daniel): this should probably be receiving resize events
     ;; from a channel and updating dynamically...
@@ -49,6 +94,7 @@
            added-meshes    (:geometry      update)
            camera-pos      (:camera        update)
            modified-meshes (:modifications update)
+           transforms      (:transforms    update)
            registered      (merge registered added-meshes)]
 
        ;;(.log js/console "update: " (prn-str update))
@@ -62,6 +108,7 @@
            ;;(.log js/console "camera update: " (prn-str axis (get camera-pos axis)))
            (aset camera "position" (name axis) (get camera-pos axis))))
        
+       ;; modifications - functions applied immediately to the state of an object
        (doseq [[mesh-id, mods] (seq modified-meshes)]
          ;;(.log js/console (prn-str mesh-id))
          (when-let [mesh (get registered mesh-id)]
@@ -71,21 +118,35 @@
                (apply aset 
                       (concat 
                        [mesh] 
-                       (if (sequential? property )
+                       (if (sequential? property)
                          property
                          [property])
                        [(transform current-val)]))))))
 
+       ;; transformations - functions applied as a function of delta-time
+       (doseq [[mesh-id, fns] (seq transforms)]
+         (let [mesh (get registered mesh-id)]
+           (doseq [[property, t] (seq fns)]
+             (let [f t]
+               (swap! tns conj 
+                      (fn [t ts dts] (-> (auto-get mesh property) (f t ts dts) ((partial auto-set mesh property)))))))))
+       
        (recur registered)))
 
     ;; TODO: these sections should be replaced from something that
     ;; reads the game state over the network
     (add-single-cube render-state)
     (set-initial-camera-position render-state)
-    (util/recur-infinitely (partial rotate-fn render-state) 50)
+    ;;(util/recur-infinitely (partial rotate-fn render-state) 50)
+    (grow-shrink render-state)
 
     ;; Start the render loop
-    (render 0 0 renderer scene camera)))
+    (render {:ts         0
+             :pts        0 
+             :renderer   renderer
+             :scene      scene
+             :camera     camera
+             :transforms tns})))
 
 (util/append-onload 
  (fn [] 
